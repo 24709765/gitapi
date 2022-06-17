5bb1f6eb867f451640f03520a7b124be4ba006f6
alvaro tintaya <atintayachuquicondo@gmail.com>
2022-06-17 00:27:01 -0500

    final

--- js.js
+++ js.js
@@ -15,10 +15,37 @@ function click(){
 
 
 async function api(inpu){
-    let response = await fetch('https://api.github.com/users/maxsoll159');
-    let usuarios = await response.json();
-    console.log(usuarios);
-
+   
+    if(inpu === undefined){
+      let response = await fetch(`https://api.github.com/users/24709765`);
+      let usuarios = await response.json();
+      console.log(usuarios);
+      pintar(usuarios);
+    }else{
+      let response = await fetch(`https://api.github.com/users/${inpu}`);
+      let usuarios = await response.json();
+      console.log(usuarios);
+      pintar(usuarios);
+    }
 }
 
+api()
+
+function pintar(usuarios){
+   const foto = document.querySelector("#foto");
+   foto.innerHTML=` 
+   <img  
+   src="${usuarios.avatar_url}" class="col-3" class=" img rounded-circle " id="hola" alt="">
+
+  <div class=" col-6">
+      <h2 class="guille">${usuarios.name}</h2>
+      <h6>@${usuarios.bio}</h6>>
+      <p class="fron"> front end- developer</p>
+  
+</div>
+  <div class="col-2">
+      <p>${usuarios.created_at}</p>
+  </div>`
+  
+}
 
